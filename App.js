3d30d839651f6edac9052788912d9e7242862223
Matheus Augusto Daminelli <125694923+daminellis@users.noreply.github.com>
2024-04-03 17:33:29 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
+import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import 'react-native-gesture-handler';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHome, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
 
-import {SafeAreaView, Text, Image, View, Button, StyleSheet} from 'react-native';
-import Profile from './Components/Profile';
-import Home from './Components/Home';
-import Login from './Components/Login';
+import Home from './Screens/Home';
+import Login from './Screens/Login';
+import Carrinho from './Screens/Carrinho';
+import Notif from './Screens/Notif';
+
+function HomeTabs() {
+    const Tab = createBottomTabNavigator();
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        iconName = faHome;
+                    } else if (route.name === 'Carrinho') {
+                        iconName = faShoppingCart; 
+                    }
+                    return <FontAwesomeIcon icon={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: '#C71585',
+                tabBarInactiveTintColor: 'gray',
+            })}>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Carrinho" component={Carrinho} />
+        </Tab.Navigator>
+    );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-
-   const Stack = createStackNavigator();
-
-  return(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Login' component={Login}/>
-        <Stack.Screen name='Home' component={Home}/>
-        <Stack.Screen name='Profile' component={Profile}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen options={{headerShown:false}} name='Home' component={HomeTabs} />
+                <Stack.Screen name='Notificação' component={Notif} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
